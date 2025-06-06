9f01238e0e15eff549730b8057dc11fec69aa91c
Nithin1418 <mrnithin1418@gmail.com>
2025-06-07 04:05:02 +0530

    updates

--- src/components/Video/video.js
+++ src/components/Video/video.js
@@ -5,6 +5,8 @@ import './video.css';
 function Video() {
     const { scrollYProgress } = useScroll();
     const opacity = useTransform(scrollYProgress, [0, 0.1], [1, 0]);
+    const display = useTransform(scrollYProgress, [0, 0.1], ['block', 'none']);
+    const videoOpacity = useTransform(scrollYProgress, [0, 0.1], [1, 0]);
 
     useMotionValueEvent(scrollYProgress, "change", (latest) => {
         // console.log("Scroll Progress:", latest);
@@ -13,7 +15,7 @@ function Video() {
     return (
         <section className="parallax">
             <div className="main-div">
-                <video
+                <motion.video
                     autoPlay
                     loop
                     muted
@@ -21,11 +23,12 @@ function Video() {
                     preload="auto"
                     className="bg-video"
                     title="Background Video"
+                    style={display}
                 >
                     <source src="/assets/video/bg.mp4" type="video/mp4" />
-                </video>
+                </motion.video>
                 <motion.div className="content" style={{ opacity }}>
-                    <h1 className="name font-mulish" style={{fontWeight:800}}>Hi, I'm Arun</h1>
+                    <h1 className="name font-mulish" style={{ fontWeight: 800 }}>Hi, I'm Arun</h1>
                     <h4 className="brand">Videographer • Editor • Visual Creator</h4>
                 </motion.div>
             </div>
